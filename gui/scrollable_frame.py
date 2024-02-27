fe9100d6eea6e57f11067df0a9d6c68da4961024
SmAlios <aloissouris@gmail.com>
2024-02-27 15:18:28 +0100

    0.1.4 - wider scrollbar

--- gui/scrollable_frame.py
+++ gui/scrollable_frame.py
@@ -5,40 +5,7 @@ from platform import system
 
 fp = functools.partial
 os = system()
-
-class ScrollableFrame(tk.Frame):
-    def __init__(self, master, *args, **kwargs):
-        tk.Frame.__init__(self, master, *args, **kwargs)
-
-        # Create a canvas and scrollbar
-        self.canvas = tk.Canvas(self)
-        self.scrollbar = tk.Scrollbar(
-            self, orient="vertical", command=self.canvas.yview
-        )
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
-
-        # Use a frame inside the canvas to hold the items
-        self.item_frame = tk.Frame(self.canvas)
-        self.item_frame.pack(fill="both", expand=True)
-
-        # Pack the canvas and scrollbar
-        self.canvas.pack(side="left", fill="both", expand=True)
-        self.scrollbar.pack(side="right", fill="both")
-
-        # Configure scrolling
-        self._item_frame_id = self.canvas.create_window(
-            self.canvas.winfo_width() / 2, 0, anchor="nw", window=self.item_frame
-        )
-
-        self.item_frame.bind("<Configure>", self.on_frame_configure)
-        self.canvas.bind("<Configure>", self.on_canvas_configure)
-
-    def on_frame_configure(self, event):
-        # Update the scroll region of the canvas when the size of the item frame changes
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-
-    def on_canvas_configure(self, event):
-        self.canvas.itemconfigure(self._item_frame_id, width=self.canvas.winfo_width())
+expanded_scrollbar = True
 
 #This code come from the JackTheEngineer/ScrolledFrame.py's Github page
 #It allow the scrollableFrame to be actually scrollable with mousewheel 
@@ -46,9 +13,14 @@ class ScrollFrame(ttk.Frame):
     def __init__(self, parent, *args, **kw):
         ttk.Frame.__init__(self, parent, *args, **kw)
 
+        if expanded_scrollbar:
+            i = 1
+        else:
+            i = 0
+
         # create a canvas object and a vertical scrollbar for scrolling it
         self.vscrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
-        self.vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
+        self.vscrollbar.pack(fill=tk.BOTH, side=tk.RIGHT, expand=i)
         self.canvas = tk.Canvas(self, bd=0, highlightthickness=0,
                             yscrollcommand=self.vscrollbar.set)
         self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
